f99ef8294e7a3d2c96f5e4798ca3cb32e0580dd7
+kelvin <kevin.mutua@student.moringaschool.com>
2022-11-23 09:10:39 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
-// Write your code here
+let num1=62
+let num2=1
+let multiply=num1*num2;
+//
+     
+const random = Math.floor(Math.random() * 10) + 1;
+let num3=4;
+let num4=5;
+let mod=num3%num4;
+//
+let max=Math.max(7,8,6,20,6);
+console.log(max)
